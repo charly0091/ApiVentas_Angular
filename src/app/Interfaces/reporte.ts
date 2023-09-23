c0f0188b44d8d529a9b25c9b4a762803618ae418
Charly <charly91.3ds@gmail.com>
2023-09-23 11:47:25 -0300

    interfaces creadas

---
+++ src/app/Interfaces/reporte.ts
@@ -0,0 +1,10 @@
+export interface Reporte {
+    numeroDocumento: string,
+    tipoPago: string,
+    fechaRegistro: string,
+    totalVenta: number,
+    producto: string,
+    cantidad: number,
+    precio: string
+    total: string
+}
